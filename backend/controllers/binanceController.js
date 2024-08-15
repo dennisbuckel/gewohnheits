13ee9e838e8ebf9c056518c60feceeee2df908bd
dennisbuckel <dennis.buck@web.de>
2024-08-15 15:00:58 +0200

    optimierungen 11

--- backend/controllers/binanceController.js
+++ backend/controllers/binanceController.js
@@ -18,14 +18,14 @@ exports.getAccountInfo = async (req, res) => {
 };
 
 exports.getTotalBalance = async (req, res) => {
-  try {
-    const accountInfo = await client.accountInfo();
-    const totalBalance = accountInfo.balances.reduce((acc, balance) => {
-      return acc + parseFloat(balance.free) + parseFloat(balance.locked);
-    }, 0);
-    res.json({ totalBalance });
-  } catch (error) {
-    console.error('Error fetching total balance:', error);
-    res.status(500).json({ message: 'Error fetching total balance' });
-  }
-};
+    try {
+      const accountInfo = await client.accountInfo();
+      const totalBalance = accountInfo.balances.reduce((acc, balance) => {
+        return acc + parseFloat(balance.free) + parseFloat(balance.locked);
+      }, 0);
+      res.json({ totalBalance });
+    } catch (error) {
+      console.error('Error fetching total balance:', error);
+      res.status(500).json({ message: 'Error fetching total balance' });
+    }
+  };
